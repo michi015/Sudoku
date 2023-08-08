0511368d2c7dc1c083d6353628d5e5cb655f0787
michi015 <michi.mathis@gmx.at>
2023-08-08 08:05:46 +0200

    still wip

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-//selectNumber + make background number
-let numSelected = null;
-function selectNumber() {
-  if (numSelected != null) {
-    numSelected.classList.remove("number-selected");
-  }
-  //TO DO: deselect number?
-  numSelected = this;
-  numSelected.classList.add("number-selected");
-}
-
-function addNumber() {
-  if (numSelected) {
-    this.value = numSelected.id;
-
-    // "0-0" "0-1"
-    let coords = this.id.split("-"); //["0", "0"]
-    let r = parseInt(coords[0]);
-    let c = parseInt(coords[1]);
-  }
-  if (solution[r][c] == numSelected.id) {
-    inputElement.classList.add("board-start");
-    console.log("test");
-  } else {
-    inputElement.classList.remove("board-start");
-  }
-}
-
 //UNDO one step - not working:
 
 // let inputElement = document.querySelector("inputElement");
--- board.js
+++ board.js
@@ -1,7 +1,9 @@
-const gameBoard = document.querySelector("#gameboard");
-const squares = 9;
+var numSelected = null;
+//var tileSelected = null;
 
-let board = [
+var errors = 0;
+
+var board = [
   "--74916-5",
   "2---6-3-9",
   "-----7-1-",
@@ -13,7 +15,7 @@ let board = [
   "81--45---",
 ];
 
-let solution = [
+var solution = [
   "387491625",
   "241568379",
   "569327418",
@@ -25,38 +27,71 @@ let solution = [
   "812945763",
 ];
 
-//grid and lines
-for (let r = 0; r < squares; r++) {
-  for (let c = 0; c < squares; c++) {
-    const inputElement = document.createElement("input");
-    // inputElement.setAttribute("type", "text");
-    // inputElement.setAttribute("maxlength", "1");
-    // inputElement.id = r.toString() + " - " + c.toString();
-    if (board[r][c] != "-") {
-      inputElement.value = board[r][c];
-      inputElement.classList.add("board-start");
-    }
-    if (r == 2 || r == 5) {
-      inputElement.classList.add("horizontal-line");
-    }
-    if (c == 2 || c == 5) {
-      inputElement.classList.add("vertical-line");
-    }
-    inputElement.addEventListener("click", addNumber);
-    // inputElement.addEventListener("onkeydown", saveMemento);
-    gameBoard.appendChild(inputElement);
-  }
-}
+window.onload = function () {
+  setGame();
+};
 
-//zahlen unter dem board
 function setGame() {
+  // Digits 1-9
   for (let i = 1; i <= 9; i++) {
+    //<div id="1" class="number">1</div>
     let number = document.createElement("div");
     number.id = i;
     number.innerText = i;
     number.addEventListener("click", selectNumber);
     number.classList.add("number");
-    document.querySelector(".numbers").appendChild(number);
+    document.getElementById("digits").appendChild(number);
+  }
+
+  // Board 9x9
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      let tile = document.createElement("div");
+      tile.id = r.toString() + "-" + c.toString();
+      if (board[r][c] != "-") {
+        tile.innerText = board[r][c];
+        tile.classList.add("tile-start");
+      }
+      if (r == 2 || r == 5) {
+        tile.classList.add("horizontal-line");
+      }
+      if (c == 2 || c == 5) {
+        tile.classList.add("vertical-line");
+      }
+      tile.addEventListener("click", selectTile);
+      tile.classList.add("tile");
+      document.getElementById("board").append(tile);
+    }
   }
 }
-setGame();
+
+function selectNumber() {
+  if (numSelected != null) {
+    numSelected.classList.remove("number-selected");
+  }
+  numSelected = this;
+  numSelected.classList.add("number-selected");
+}
+
+function selectTile() {
+  if (numSelected) {
+    // if (this.innerText != "") {
+    //   return;
+    // }
+
+    if (this.classList.contains("tile-start")) {
+      return;
+    }
+
+    let coords = this.id.split("-"); //"0-0" "0-1" > ["0", "0"]
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    if (solution[r][c] == numSelected.id) {
+      this.innerText = numSelected.id;
+      this.classList.add("tile-right");
+    } else {
+      this.innerText = numSelected.id;
+    }
+  }
+}
